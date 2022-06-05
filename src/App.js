51cda496f24535565cfc2f2cca366d0b03a03ba4
Vlad Shalandin <vladislav.shalandin@gmail.com>
2022-06-05 18:00:02 -0400

    fetch data function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
 
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const jobs = await response.json();
+    setJobs(jobs);
+    setIsLoading(false);
+    console.log(jobs);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
   return <h2>tabs project setup</h2>;
 }
 
